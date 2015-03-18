c1072b73f35a540f56335afe8785d1a6a7bf9784
jesspaget <jessicapaget@hotmail.com>
2015-03-18 16:15:22 -0400

    Button fixes.
    Changed my tabs to buttons and fixed the JS to make them fade in and
    out.

--- js/general.js
+++ js/general.js
@@ -1,10 +1,27 @@
-var $tabs = $('.tabs');
-var $panels = $('.panel');
+var $artShowOne = $('.artimg1');
+var $btnShowOneHide = $('.firstbutton');
 
-$tabs.on('click', 'a', function (e) {
-    e.preventDefault();
-    var id = $(this).attr('href');
-    
-    $panels.filter(':not([hidden])').attr('hidden', true);
-    $(id).removeAttr('hidden');
+var $artShowTwo = $('.artimg2');
+var $btnShowTwoHide = $('.secbutton');
+
+var $artShowThree = $('.artimg3');
+var $btnShowThreeHide = $('.trdbutton');
+
+$btnShowOneHide.on('click', function () {
+    $artShowTwo.removeClass('js-toggle-show');
+    $artShowThree.removeClass('js-toggle-show');
+    $artShowOne.toggleClass('js-toggle-show');
 });
+
+$btnShowTwoHide.on('click', function () {
+    $artShowOne.removeClass('js-toggle-show');
+    $artShowThree.removeClass('js-toggle-show');
+    $artShowTwo.toggleClass('js-toggle-show');
+});
+
+$btnShowThreeHide.on('click', function () {
+    $artShowTwo.removeClass('js-toggle-show');
+    $artShowOne.removeClass('js-toggle-show');
+    $artShowThree.toggleClass('js-toggle-show');
+});
+
